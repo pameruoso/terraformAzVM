c13bdbb33ba483528e9d1ea14287169c9b14060c
Pietro Ameruoso <42093926+pameruoso@users.noreply.github.com>
2023-02-16 14:20:06 +0100

    Update azvm.tf

--- azvm.tf
+++ azvm.tf
@@ -29,6 +29,8 @@ resource "azurerm_public_ip" "my_terraform_public_ip" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
+  
+  __
 }
 
 # Create Network Security Group and rule
