0020498e9417ad989e6eefa2caa956932282eadd
Pietro Ameruoso <42093926+pameruoso@users.noreply.github.com>
2023-02-16 09:25:55 +0100

    Update azvm.tf

--- azvm.tf
+++ azvm.tf
@@ -119,7 +119,7 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   computer_name                   = "myvm"
   admin_username                  = "azureuser"
   disable_password_authentication = false
-  password = "myfakepassword"
+  admin_password = "myfakepassword"
 
   boot_diagnostics {
     storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
